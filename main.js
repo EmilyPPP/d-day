5989359c4bbf48f0320aff5e2f0304fe97ce4b23
Emily <119949731+EmilyPPP@users.noreply.github.com>
2024-04-01 16:54:27 +0900

    feat: add notion dday widget

---
+++ main.js
@@ -0,0 +1,42 @@
+const getUrlParams = () => {
+  const params = {};
+  window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, (_, key, value) => {
+    params[key] = value;
+  });
+
+  return params;
+};
+
+const params = getUrlParams();
+document.getElementById('title').innerHTML =
+  params?.title?.replace('_', ' ') ?? '목표 달성일';
+
+const endDate = new Date(params.date?.split('-'));
+if (endDate) {
+  document.getElementById('endDate').innerHTML = `${endDate.toLocaleDateString(
+    'ko-KR'
+  )} (${endDate.toLocaleDateString('en-US', { weekday: 'short' })})`;
+}
+
+let dday = setInterval(() => {
+  const startDate = new Date();
+  startDate.setHours(0);
+  startDate.setMinutes(0);
+  startDate.setSeconds(0);
+  startDate.setMilliseconds(0);
+
+  const diff = endDate - startDate;
+
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  document.getElementById('count').innerHTML = getDday(days);
+}, 60);
+
+const getDday = (days) => {
+  if (days === 0) {
+    return 'D-Day';
+  } else if (days < 0) {
+    return `D+${-1 * days}`;
+  } else {
+    return `D-${days}`;
+  }
+};
